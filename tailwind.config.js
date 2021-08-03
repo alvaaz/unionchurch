b958128a09cfb4518e7cb0503fdc9cdde2796c83
Álvaro Göede Rivera <alvaro@goede.cl>
2021-08-03 18:04:11 -0400

    tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './pages/**/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/**/*.{js,ts,jsx,tsx}',
+    './components/**/**/**/*.{js,ts,jsx,tsx}',
+    './components/**/**/**/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false,
   theme: {
     fontFamily: {
