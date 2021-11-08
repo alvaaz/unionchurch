addbeb43a3eba876d69e474af887663baa172642
Álvaro Göede Rivera <alvaro@goede.cl>
2021-11-08 17:37:14 -0300

    backend logo

---
+++ backend/admin/config.tsx
@@ -0,0 +1,25 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "@keystone-ui/core";
+import { AdminConfig } from "@keystone-next/keystone/types";
+import Link from "next/link";
+import Image from "next/image";
+
+export const CustomLogo = () => {
+  return (
+    <Link href="/" passHref>
+      <Image
+        src="/logo-white.svg"
+        width={146}
+        height={73}
+        alt="logo"
+        layout="fixed"
+      />
+    </Link>
+  );
+};
+
+// Presently the Logo is the only Admin UI component that is customisable.
+export const components: AdminConfig["components"] = {
+  Logo: CustomLogo,
+};
