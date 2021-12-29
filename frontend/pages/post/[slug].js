df9134632ddea141e439f6af43552f125f41a46a
alvaaz <alvaro@goede.cl>
2021-12-29 18:22:02 -0300

    delete comments

--- frontend/pages/post/[slug].js
+++ frontend/pages/post/[slug].js
@@ -7,7 +7,6 @@ import { dateTransform } from '../../lib';
 import { initializeApollo } from '../../lib/apolloClient';
 
 const renderers = {
-  // Render heading blocks
   block: {
     heading({ level, children, textAlign }) {
       const Comp = `h${level}`;
