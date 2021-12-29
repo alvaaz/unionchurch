519aef75292a8a9b76a6bc5204f8631da1a1ba8c
alvaaz <alvaro@goede.cl>
2021-12-29 15:48:59 -0300

    graphql schema isn't up to date

--- backend/schema.ts
+++ backend/schema.ts
@@ -11,13 +11,7 @@ export const lists = {
   Person: list({
     fields: {
       name: text({validation: { isRequired: true }}),
-      // Added an email and password pair to be used with authentication
-      // The email address is going to be used as the identity field, so it's
-      // important that we set both isRequired and isUnique
       email: text({validation: { isRequired: true}, isIndexed: 'unique'}),
-      // The password field stores a hash of the supplied password, and
-      // we want to ensure that all people have a password set, so we use
-      // the isRequired flag.
       password: password({ validation: { isRequired: true }}),
     },
   }),
