00bf7ea3a58e88a26b20108ec2cf617ddb633e22
Álvaro Goede <alvaro@goede.cl>
2022-12-10 19:31:07 -0300

    add databas url

--- backend/config.ts
+++ backend/config.ts
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const PORT = parseInt(process.env.PORT!) || 3000;
 
-export const DATABASE_URL = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}`;
+export const DATABASE_URL = process.env.DATABASE_URL || `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}/${process.env.PGDATABASE}`;
 
 export const SESSION_MAX_AGE = parseInt(process.env.SESSION_MAX_AGE!) || 60 * 60 * 24 * 30;
 
